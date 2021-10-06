6af8e037ad6feaecf7fba5ce873d576348814c38
Gustavo Garcia Gonzalez <ggarciag@vector.com>
2021-10-06 04:49:14 +0200

    create persistence object and script migration

---
+++ src/main/java/com/companies/entities/PriceEntity.java
@@ -0,0 +1,43 @@
+package com.companies.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "prices")
+public class PriceEntity {
+    @Id
+    @Column(name = "price_list", nullable = false)
+    private Integer priceList;
+
+    @Column(name = "brand_id", nullable = false)
+    private Integer brandId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "product_id", nullable = false)
+    private Integer productId;
+
+    @Column(name = "priority", nullable = false)
+    private Integer priority;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "curr", nullable = false)
+    private String curr;
+}
--- src/main/resources/db/migration/V1__0_create_bbdd.sql
+++ src/main/resources/db/migration/V1__0_create_bbdd.sql
@@ -11,3 +11,8 @@ CREATE TABLE PUBLIC.PRICES
     PRICE      DOUBLE     NOT NULL,
     CURR       VARCHAR(3) NOT NULL
 );
+INSERT INTO PUBLIC.PRICES (BRAND_ID, START_DATE, END_DATE, PRICE_LIST, PRODUCT_ID, PRIORITY, PRICE, CURR )
+VALUES (1, parsedatetime('2020-06-14-00.00.00', 'yyyy-MM-dd-hh.ss.SS'), parsedatetime('2020-12-31-23.59.59', 'yyyy-MM-dd-hh.ss.SS'), 1, 35455, 0, 35.50, 'EUR'),
+       (1, parsedatetime('2020-06-14-15.00.00', 'yyyy-MM-dd-hh.ss.SS'), parsedatetime('2020-06-14-18.30.00', 'yyyy-MM-dd-hh.ss.SS'), 2, 35455, 1, 25.45, 'EUR'),
+       (1, parsedatetime('2020-06-15-00.00.00', 'yyyy-MM-dd-hh.ss.SS'), parsedatetime('2020-06-15-11.00.00', 'yyyy-MM-dd-hh.ss.SS'), 3, 35455, 1, 30.50, 'EUR'),
+       (1, parsedatetime('2020-06-15-16.00.00', 'yyyy-MM-dd-hh.ss.SS'), parsedatetime('2020-12-31-23.59.59', 'yyyy-MM-dd-hh.ss.SS'), 4, 35455, 1, 38.95, 'EUR');
